75e54fe4fd1220dff175297f5b6888707b43f757
Nastyagulitskaya <101734369+Nastyagulitskaya@users.noreply.github.com>
2022-05-04 22:41:54 +0600

    Add files via upload

---
+++ skript4.js
@@ -0,0 +1,11 @@
+// функция fun1() {
+// var sel = document.getElementById('mySelect').SelectedIndex;
+// var options = document.getElementById('mySelect').options;
+// alert('Выбрана опция ' +options[sel].text);
+// }
+
+
+функция fun1() {
+    var rng = document.getElementById('r1');
+    var div = document.getElementById('test');
+    div.style.width=rng.value+'px'; }
